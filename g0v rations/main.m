d188d92d97399333e92cc33f3ec46452f7ae846e
AWS Server <668jerry@gmail.com>
2014-08-28 23:43:20 +0800

    Initial Commit

---
+++ g0v rations/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  g0v rations
+//
+//  Created by lololol on 28/Aug/14.
+//  Copyright (c) 2014 Leisure Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ g0v rationsTests/g0v_rationsTests.m
@@ -0,0 +1,39 @@
+//
+//  g0v_rationsTests.m
+//  g0v rationsTests
+//
+//  Created by lololol on 28/Aug/14.
+//  Copyright (c) 2014 Leisure Design. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface g0v_rationsTests : XCTestCase
+
+@end
+
+@implementation g0v_rationsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
